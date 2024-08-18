ca576618f797a179af1fa6463dd7774660fddce9
slash-l <slash_mumu@163.com>
2024-08-18 20:55:13 +0800

    init bosch code.

---
+++ src/main/java/com/jfrogchina/soleng/callback/AppSpringBootApplication.java
@@ -0,0 +1,32 @@
+package com.jfrogchina.soleng.callback;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class AppSpringBootApplication {
+
+	private static final Logger logger = LoggerFactory.getLogger(AppSpringBootApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpringBootApplication.class, args);
+	}
+
+	@GetMapping("/")
+	public String helloAppBosch(){
+		logger.info("This is the main index request.");
+		return "Hello Spring boot!";
+	}
+
+	@PostMapping(value = "/callbackELM")
+	public String callbackELM(){
+		logger.info("--------------- start callbackELM ------------------------");
+		return "Hello ELM";
+	}
+}
---
+++ src/test/java/com/jfrogchina/soleng/callback/AppSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jfrogchina.soleng.callback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
